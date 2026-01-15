3d475a10fbb42caa738b525c748c678e88b38229
iritpaiement <i.e.paiement@wustl.edu>
2026-01-14 19:08:47 -0600

    robot tree

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,31 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.forward(60);
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnLeft(90);
+		robot.forward(120);
+		robot.turnRight(90);
+		robot.forward(180);
+		robot.turnRight(90);
+		robot.forward(350);
+		robot.turnRight(90);
+		robot.forward(180);
+		robot.turnRight(90);
+		robot.forward(90);
+		robot.turnLeft(90);
+		robot.forward(90);
+		robot.turnRight(90);
+		robot.forward(90);
+		robot.turnLeft(90);
+		robot.forward(90);
+
+
+
+
 	}
 
 }
